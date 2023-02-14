da94837f909370da76c7449bf7f66210a410c880
coalexa <coalexa98@gmail.com>
2023-02-14 15:06:40 -0800

    added PS1 parameter before getting input

--- smallsh.c
+++ smallsh.c
@@ -5,11 +5,15 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <sys/types.h> //might not need? someone said for ssize_t
+
+void prompt_function(); //function for PS1 prompt
 
 int main(int argc, char *argv[]){
   char *line = NULL;
   size_t n = 0;
   for(;;) {
+    prompt_function();
     ssize_t line_length = getline(&line, &n, stdin);
 
     if(line_length == -1) {
@@ -18,5 +22,14 @@ int main(int argc, char *argv[]){
       printf("it works?");
       exit(0);
     }
+  }
+}
+
+void prompt_function(){
+  char *ps_parameter = getenv("PS1");
+  if(ps_parameter == NULL) {
+    fprintf(stderr, "");
+  } else {
+    fprintf(stderr, "%s", ps_parameter);
   }
 }
