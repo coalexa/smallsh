2e987267a6b071962b844fc72c9179f8162acb66
coalexa <coalexa98@gmail.com>
2023-02-14 14:19:56 -0800

    added getline function for user input

--- smallsh.c
+++ smallsh.c
@@ -4,3 +4,19 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]){
+  char *line = NULL;
+  size_t n = 0;
+  for(;;) {
+    ssize_t line_length = getline(&line, &n, stdin);
+
+    if(line_length == -1) {
+      exit(1);
+    } else {
+      printf("it works?");
+      exit(0);
+    }
+  }
+}
